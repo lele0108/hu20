899c62d26fdaedc459d88a9fedc907904600763d
Jimmy Liu <sicong.liu98@gmail.com>
2014-01-19 14:00:15 -0800

    About page, etc.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function() {
 
-
+	
 	$('.header').videoBG({
       position:"relative",
-    zIndex:-1,
+    zIndex:0,
   mp4:'http://d385d9sgj3wiu1.cloudfront.net/mhacks.mp4',
   ogv:'http://d385d9sgj3wiu1.cloudfront.net/mhacks.ogg',
   webm:'http://d385d9sgj3wiu1.cloudfront.net/mhacks.ogg',
